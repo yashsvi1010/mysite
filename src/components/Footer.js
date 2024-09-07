c40c3a9ac006dd660aaa98d4254e96a193cbc40c
yashsvi1010 <yashsvisingh97@gmail.com>
2024-09-07 17:14:16 +0530

    created site structure

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="content-footer ">
+            <h5>Copyright © 2024. All rights are reserved</h5>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Project.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <>
+      <section id="projects">
+        <div className="container">
+          <div className="content proj-content">
+            <div className="items">
+              <div className="about-img">Image</div>
+              <div className="about-text">
+                <h4>Project 1</h4>
+                <p>
+                  Description- Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit. Ut nobis obcaecati molestias labore nihil
+                  molestiae corporis nostrum quo numquam veniam exercitationem
+                  quae earum sunt fugiat similique consectetur, debitis quis
+                  facilis?
+                </p>
+                <p>
+                  Hey, my name is Yashsvi, and I'm a Frontend Developer. My
+                  passion is to create and develop a clean UI/UX for my users.
+                  My main stack currently is ReactJs in combination with
+                  JavaScript CSS and HTML5.
+                </p>
+                <div className="skills-use">
+                  <ul>
+                    <li>React</li>
+                    <li>JS</li>
+                    <li>CSS</li>
+                  </ul>
+                </div>
+              </div>
+              {/* Project -2 */}
+              <div className="about-img">Image</div>
+              <div className="about-text">
+                <h4>Project 2</h4>
+                <p>
+                  Description- Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit. Ut nobis obcaecati molestias labore nihil
+                  molestiae corporis nostrum quo numquam veniam exercitationem
+                  quae earum sunt fugiat similique consectetur, debitis quis
+                  facilis?
+                </p>
+                <p>
+                  Hey, my name is Yashsvi, and I'm a Frontend Developer. My
+                  passion is to create and develop a clean UI/UX for my users.
+                  My main stack currently is ReactJs in combination with
+                  JavaScript CSS and HTML5.
+                </p>
+                <div className="skills-use">
+                  <ul>
+                    <li>React</li>
+                    <li>JS</li>
+                    <li>CSS</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Projects;
